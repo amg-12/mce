f2e70cdb9c06f9639594bb35b64b4952af1a172f
Amitai <git@amitai.ga>
2023-10-30 12:36:59 +0000

    start work on selector block

--- extension.ts
+++ extension.ts
@@ -1,22 +1,47 @@
-/**
- * Functions are mapped to blocks using various macros
- * in comments starting with %. The most important macro
- * is "block", and it specifies that a block should be
- * generated for an **exported** function.
- */
+enum Rule {
+    //% block="max radius (r)"
+    MaxRadius,
+    //% block="min radius (rm)"
+    MinRadius,
+    //% block="tag"
+    Tag,
+    //% block="name"
+    Name,
+    //% block="type"
+    Type,
+    //% block="has item"
+    HasItem,
+    //% block="gamemode (m)"
+    Gamemode,
+    //% block="limit (c)"
+    Limit
+}
 
-//% color="#AA278D" weight=100
-namespace hello {
-    //% block
-    export function helloWorld() {
-
+function ruleString(rule: Rule): string {
+    switch (rule) {
+        case Rule.MaxRadius: return "r";
+        case Rule.MinRadius: return "rm"
+        case Rule.Tag: return "tag";
+        case Rule.Name: return "name";
+        case Rule.Type: return "type";
+        case Rule.HasItem: return "hasitem";
+        case Rule.Gamemode: return "m";
+        case Rule.Limit: return "c";
+        default: return "";
     }
+}
 
-    // note that Caml casing yields lower case
-    // block text with spaces
-
-    //% block
-    export function camlCaseTwo() {
-
+//% weight=100 color=#33bbff icon="\uf135"
+namespace codeZone {
+    /**
+     * TODO: describe your function here
+     * @param n describe parameter here, eg: 5
+     * @param s describe parameter here, eg: "Hello"
+     * @param e describe parameter here
+     */
+    //% block="%target=minecraftTarget with $rule = $value"
+    export function foo(target: TargetSelector, rule: Rule, value: string): TargetSelector {
+        target.addRule(ruleString(rule), value);
+        return target;
     }
 }
